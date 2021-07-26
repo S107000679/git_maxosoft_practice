9328dfb81b37e3e5819df16f3928fb66229e35e6
S107000679 <43431947+S107000679@users.noreply.github.com>
2021-07-26 17:18:10 +0800

    Add files via upload

---
+++ 109_ksu_maxosoft_intern/grady/Learn/info.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class = "card">
+        <!-- <div class="col-6 col-md-4"> -->
+       <div class="card-header">
+        Chart
+        </div>
+        <div class="card-body">
+            <bar :data="data" :option="option"> </bar>
+      </div>
+      
+      </div>
+    <!-- </div> -->
+    <div class = "card">
+        <!-- <div class="col-6 col-md-4"> -->
+       <div class="card-header">
+        Chart
+        </div>
+        <div class="card-body">
+            <Doughnut :data="data" :option="option"> </Doughnut>
+      </div>
+      </div>
+    <!-- </div> -->
+      
+</template>
+
+<script>
+import bar from './bar.vue';
+import Doughnut from './Doughnut.vue';
+
+
+export default{
+       name:'info',
+       props:[
+           'data',
+           'option'
+       ],
+       components:{
+           bar,
+         Doughnut
+       }
+}
+</script>
